d5daf14f84497d3521427e4b30ddf4b5726041e6
Nikhil Saraf <nsaraf98@gmail.com>
2020-04-30 15:24:48 -0400

    fix templates

--- templates/react/lib/index.tsx
+++ templates/react/lib/index.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
+
 export const sum = (a: number, b: number) => {
   if (process.env.NODE_ENV === 'development') {
     console.log('boop');
   }
   return a + b;
+};
+
+export const Thing = () => {
+  return <div>Hello world</div>;
 };
